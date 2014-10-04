8dbe2e9cb4de881a10ec6ce13855aaa631a17cf4
FRC2404 <2404Robotics@outlook.com>
2014-10-03 23:25:52 -0700

    Need to work on this, once everything else is tested

--- sparky/Auton.h
+++ sparky/Auton.h
@@ -1,28 +1,3 @@
 /*
-#ifndef _Auton_h_
-#define _Auton_h_
-
-#include "WPILib.h"
-#include "myRobot.h"
-
-class Auton
-{
-public:
-	Auton(){};
-	~Auton(){};
-	
-	struct mecanum {
-		void forward(float s, float t){
-			
-		};
-	};
-	
-	struct tank {
-		void forward(float s, float t){
-			
-		}
-	};
-};
-
-#endif
+		Nothing here yet....
 */
